8fb1de4ca054b497d06dbab4ddeaa35f334f5013
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2023-09-19 09:00:04 +0000

    Aggiornamento

--- Voice_Recorder.py
+++ Voice_Recorder.py
@@ -3,9 +3,15 @@
 #sudo apt-get install libportaudio2
 #pip install pipwin
 #pipwin install pyaudio
+#python -m pip install sounddevice
+#pip install scipy
+#pip install pipwin
+#pipwin install pyaudio
 import sounddevice
 from scipy.io.wavfile import write
 
+second =  int(input("Enter time duration in seconds: "))
+fs = 44100
 record_voice = sounddevice.rec( int( second * fs ) , samplerate = fs , channels = 2 )
 sounddevice.wait()
 record_voice= sounddevice.rec(second* fs, dtype='float64')
@@ -13,13 +19,4 @@ sounddevice.query_devices()
 sounddevice.default.device = 'digital output'
 write("out.wav", fs , record_voice )
 
-
-import sounddevice
-from scipy.io.wavfile import write
-fs= 44100
-second =  int(input("Enter time duration in seconds: "))
-print("Recording.....n")
-record_voice = sounddevice.rec( int ( second * fs ) , samplerate = fs , channels = 2 )
-sounddevice.wait()
-write("out.wav",fs,record_voice)
 print("Finished.....nPlease check your output file")
